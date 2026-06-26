c2513ba6a75f1ee74a44ad7d5296c033420a9afe
Sithum Bimsara <sithumbimsara2001@gmail.com>
2026-06-26 12:35:54 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/homePage'
+import AdminPage from './pages/adminPage'
+import ProductsPage from './pages/productsPage'
+import TestPage from './pages/test'
+import LoginPage from './pages/loginPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
   return (
-    <>
-      <div>
-          Sithum Bimsara
+      <div className='w-full h-screen flex justify-center items-center bg-primary text-secondary'>
+        <Toaster position='top-right'/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/admin/*' element={<AdminPage/>}/>
+          <Route path='/products/*' element={<ProductsPage/>}/>
+          <Route path='/test' element={<TestPage/>}/>
+        </Routes>
       </div>
-      
-    </>
   )
 }
 
