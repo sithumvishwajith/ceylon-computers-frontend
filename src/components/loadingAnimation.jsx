84a32aa6013b931f0d32df1db90dcbc220793b6c
Sithum Bimsara <sithumbimsara2001@gmail.com>
2026-06-28 23:00:54 +0530

    create loadingAnimation.jsx

---
+++ src/components/loadingAnimation.jsx
@@ -0,0 +1,8 @@
+export default function LoadingAnimation(){
+    return(
+        <div className="w-full h-[300px] flex justify-center items-center">
+            <div className="w-[80px] h-[80px] border-6 border-accent border-b-secondary rounded-full animate-spin">
+            </div>
+        </div>
+    )
+}
