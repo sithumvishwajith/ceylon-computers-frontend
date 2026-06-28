5addeaa441fe8459ca5f24609ab122c9f7c0f46c
Sithum Bimsara <sithumbimsara2001@gmail.com>
2026-06-28 11:16:28 +0530

    create homePage.jsx

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+
+export default function HomePage(){
+    return(
+        <div className='w-full h-screen flex justify-center'>
+            <Header/>
+        </div>
+    )
+}
+
+
